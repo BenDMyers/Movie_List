52dcf42d2d51b1d71f6b930fcebadaa644ed8e7b
Ben Myers <ben.myers@okstate.edu>
2019-07-14 11:40:16 -0500

    Add functional skiplinks

---
+++ src/components/layout/SkipLink.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+
+import './SkipLink.styles.css';
+
+/**
+ * A hidden button that displays when focused and, when clicked, focuses the user on some given element.
+ */
+const SkipLink = (props) => {
+    const [focused, setFocused] = useState(false);
+    const handleFocus = () => {setFocused(true);}
+    const handleBlur = () => {setFocused(false);}
+    const jump = () => {document.querySelector(`#${props.to}`).focus();}
+
+    const classes = `skip-link ${props.color} ${focused ? '' : 'screenreader'}`;
+
+    return (
+        <Button id={`skip-to-${props.to}`} className={classes} onClick={jump} onFocus={handleFocus} onBlur={handleBlur}>
+            {props.children}
+        </Button>
+    );
+}
+
+SkipLink.propTypes = {
+    /**
+     * Contents of skip link.
+     * Should describe what the button will do.
+     */
+    children: PropTypes.node.isRequired,
+    /** Button display color */
+    color: PropTypes.string.isRequired,
+    /** ID of element this link skips to */
+    to: PropTypes.string.isRequired
+};
+
+export default SkipLink;
